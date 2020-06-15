5ceb78c3bbc9c953bb18c529cbdcc4cc5a1712a4
jungmin3834 <52225582+jungmin3834@users.noreply.github.com>
2020-06-15 15:37:29 +0900

    Update React Files // Image 경로 수정

---
+++ react/src/App.js
@@ -0,0 +1,157 @@
+import React from 'react';
+import ImageMap from './imageMap';
+import './css/style.css';
+
+const TodoListTemplate = ({form, children}) => {
+  return (
+<div>
+    <div>
+          <span>SHAWN</span>
+            <ul id="gnb">
+                <li className="on">
+                    <img src={ImageMap.US} alt="s"/>
+                    <a href="#">
+                        <i className="fas fa-chevron-down"></i>
+                    </a>
+                    <ul>  
+                        <li><img alt=""  src={ImageMap.US}/><a href="#" >EN</a></li>
+                        <li><img alt=""  src={ImageMap.kr}/><a href="#">KR</a></li>
+                    </ul>
+                </li>
+                <li><a href="#">Help</a></li>
+                <li><a href="#">Login</a></li>
+                <li><a href="#" className="btn">SIGN UP</a></li>
+            </ul>
+    </div>
+    <div className="text">
+          <h2>THE CLEVER NEW WAY<br></br>TO BEAT BANK FEES</h2>
+          <p>Banks charge hidden charges when you send money abroad. With<br/></p>
+          <div className="clip">
+              <img alt=""  src={ImageMap.ap_resize} alt="img"/>
+              <a href="#">how does transferwise work?</a>
+          </div>
+    </div>
+     <div className="Mbox">
+          <div className="Numbox">
+                <div className="t1">
+                    <p>You're sending exactly</p>
+                    <h1>1,000</h1>
+                </div>
+                <div className="b1">
+                    <img alt=""  src={ImageMap.en}/>
+                    <span>GDP</span>
+                    <i className="fas fa-chevron-down"></i>
+                    <ul>
+                        <li>
+                            <img alt=""  src={ImageMap.US}/>
+                            <span>USD</span>
+                        </li>
+                        <li>
+                            <img alt=""  src={ImageMap.kr}/>
+                            <span>KRW</span>                          
+                        </li>
+                        <li>
+                            <img alt=""  src={ImageMap.en}/>
+                            <span>GDP</span>                          
+                        </li>
+                        <li>
+                            <img alt=""  src={ImageMap.ca}/>
+                            <span>CAD</span>                          
+                        </li>
+                        <li>
+                            <img alt=""  src={ImageMap.TW}/>
+                            <span>TWD</span>                           
+                        </li>
+                        <li>
+                            <img alt=""  src={ImageMap.hk}/>
+                            <span>HKD</span>                       
+                        </li>
+                    </ul>
+                </div>
+                <div className="t2">
+                    <p>Recipient gets</p>
+                    <h1>1,979</h1> <i className="fas fa-unlock"></i>
+                </div>
+                <div className="b2">
+                    <img alt=""  src={ImageMap.ca}/>
+                    <span>CAD</span>
+                    <i className="fas fa-chevron-down"></i>
+
+                    <ul>
+                        <li>
+                            <img alt=""  src={ImageMap.US}/>
+                            <span>USD</span>
+                            
+                        </li>
+
+                        <li>
+                            <img alt=""  src={ImageMap.kr}/>
+                            <span>KRW</span>                          
+                        </li>
+
+                        <li>
+                            <img alt=""  src={ImageMap.en}/>
+                            <span>GDP</span>                          
+                        </li>
+
+                        <li>
+                            <img alt=""  src={ImageMap.ca}/>
+                            <span>CAD</span>                          
+                        </li>
+
+                        <li>
+                            <img alt=""  src={ImageMap.TW}/>
+                            <span>TWD</span>                           
+                        </li>
+
+                        <li>
+                            <img alt=""  src={ImageMap.hk}/>
+                            <span>HKD</span>                       
+                        </li>
+
+                        
+                    </ul>
+
+
+                </div>
+            </div>
+            <p>Real exchange rate: <b>1.9889</b> Fee: <b>£ 4.98</b></p>
+            <p>you re saving <b>£ 44.50</b></p>
+            <button className="bt1">SEE HOW</button>
+            <button className="bt2">GET STARTED</button>
+        </div>
+        <div>
+        <div className="bg1"></div>
+        <div className="bg2"></div>
+        <div className="iconBox">
+            <div className="icon">
+                <img alt=""  src={ImageMap.Paul} alt=""/>
+                <p>Sir Richard Branson invested in</p>
+                   <a href="#">TransferWise - read why</a>  
+            </div>
+            <div className="icon">
+                <h3>★★★<b>18535</b>★★★</h3>
+                <em>REVIEWS</em>
+                <p>5 star rating on Trustpilot - See why</p>
+                    <a href="#">people love TransferWise.</a>
+            </div>
+            <div className="icon">
+                <i className="fas fa-shield-alt"></i>
+                <p>Fully authorised by the UK Financial <br></br>
+                Conduct Authority (<a href="#">The FCA</a>)
+                </p>
+                    
+            </div>
+            <div className="icon">
+                <i className="fab fa-skype"></i>
+                <p>From the people who<br/>
+                    built Skype</p>
+            </div>
+        </div>
+      </div>
+
+    </div>
+  );
+};
+
+export default TodoListTemplate;
---
+++ react/src/imageMap.js
@@ -0,0 +1,30 @@
+import ap_resize from './img/ap_resize.png';
+import ca from './img/ca.png';
+import citySilhouette from './img/city-silhouette.jpg';
+import cn from './img/CN.png';
+import en from './img/en.jpg';
+import hk from './img/HK.png';
+import kr from './img/KR.png';
+import Paul from './img/Paul_Circle.png';
+import TW from './img/TW.png';
+import US from './img/US.jpg';
+
+
+
+
+const iamgeMap = {
+    ap_resize : ap_resize,
+    ca : ca,
+    citySilhouette : citySilhouette,
+    cn : cn,
+    en : en,
+    hk : hk,
+    kr: kr,
+    Paul : Paul,
+    TW : TW,
+    US : US
+
+}
+
+
+export default iamgeMap;
